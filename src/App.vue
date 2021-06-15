870c3f3e9c64bff355097cadcbcd4abd763f568e
luvar1111 <wsp485786@gmail.com>
2021-06-15 15:27:14 +0900

    add

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <top-header/>
+  <router-view></router-view>
+  <bottom-footer/>
+</template>
+
+<script>
+import header from './components/header.vue'
+import footer from './components/footer.vue'
+export default {
+  name:'app',
+  components:{
+    'top-header':header,
+    'bottom-footer':footer
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, distinctio!
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
